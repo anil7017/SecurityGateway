faa365c42e0e16ff389d7d3dcf771405ccdf51e4
Anil kumar sahoo <anilkumarsahoo17@gmail.com>
2020-07-18 11:20:06 +0530

    add jwt implementation

--- src/main/java/com/innoverenit/SecurityGatewayApplication.java
+++ src/main/java/com/innoverenit/SecurityGatewayApplication.java
@@ -5,6 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.cloud.netflix.zuul.EnableZuulServer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
 @EnableEurekaClient
@@ -14,5 +16,9 @@ public class SecurityGatewayApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SecurityGatewayApplication.class, args);
 	}
-
+	
+	@Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder(); // For encrypting user password
+    }
 }
